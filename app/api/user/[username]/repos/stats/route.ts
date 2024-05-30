24dc13a4b7137471bf9b3b5e8187692caa9e39c9
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-30 12:54:33 -0400

    Get user's repo data

---
+++ app/api/user/[username]/repos/stats/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { Octokit } from "octokit";
+
+interface IParams {
+  username: string;
+}
+
+export async function GET(request: Request, { params }: { params: IParams }) {
+  try {
+    const { username } = params;
+    const octokit = new Octokit({});
+
+    const res = await octokit.request(`GET /users/${username}/repos`, {
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+        accept: "application/vnd.github+json",
+      },
+    });
+
+    console.log(`Found ${res.data.length} repos for ${username}`);
+
+    return NextResponse.json(res.data);
+  } catch (err) {
+    console.error("Error fetching issues:", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
